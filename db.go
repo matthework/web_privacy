4f2d54c944737478b5c2faedbd1567a7f56fec77
mattwangprop <matt.wang@propellerhead.co.nz>
2016-09-29 03:27:32 +1300

    update db

--- db.go
+++ db.go
@@ -6,12 +6,12 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
-var connectURL = "localhost"
-var DB = "privacy"
+// var connectURL = "localhost"
+// var DB = "privacy"
 
 // mlab mongodb - heroku
-// var connectURL = os.Getenv("MONGODB_URI") 
-// var DB = os.Getenv("MONGODB_DB")
+var connectURL = os.Getenv("MONGODB_URI") 
+var DB = os.Getenv("MONGODB_DB")
 
 func connect() (session *mgo.Session) {
 	session, err := mgo.Dial(connectURL)
